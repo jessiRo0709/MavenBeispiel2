399e8b40df67c441306c4df91ccb3f8b02de0dc0
Jessi <rosshart@hs-furtwangen.de>
2016-11-29 11:43:50 +0100

    src und pom hinzugefügt

---
+++ src/main/java/de/jessi/GrossUndKleinSchreibung.java
@@ -0,0 +1,18 @@
+package de.jessi;
+
+import java.util.Scanner;
+
+public class GrossUndKleinSchreibung {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Gib was ein!");
+		Scanner scanner = new Scanner(System.in);
+		String eingabe = scanner.nextLine();
+		System.out.println(eingabe.toUpperCase());
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
